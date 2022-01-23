99a6d87189ef791eecbf9b4ea9db0c6b6a6b28dd
Rodion Sibov <rodionsibov@gmail.com>
2022-01-23 18:07:41 +0200

    update

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,13 @@
-console.log('Task manager app');
+const express = require('express')
+const app = express()
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+
+
+const port = 5000
+
+app.listen(port, console.log(`Server is listening on port ${port}...`))
