610ea0d09a0cac66923476b6a0f2cbaea0e30d7d
Rodion Sibov <rodionsibov@gmail.com>
2022-01-23 18:24:20 +0200

    update

--- starter/app.js
+++ starter/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json())
+
 
 // routes
 app.get('/hello', (req, res) => {
     res.send('Task Manager App')
 })
 
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks')
 
 
 const port = 5000
